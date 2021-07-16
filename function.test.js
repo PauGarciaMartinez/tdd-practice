f10e8a60afe573fd4b62ad17f8d7ce077f642ec4
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-16 18:56:14 +0200

    Finish basic tests to practice matchers

--- function.test.js
+++ function.test.js
@@ -18,3 +18,18 @@ test('User should be Pau Garcia object', () => {
     lastName: 'Garcia'
   })
 });
+
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/i/i);
+});
+
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
